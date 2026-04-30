03e695ddaa0024525caf6607c0f5e353cc711235
NguyenNguyen <nguyenbk92@gmail.com>
2026-04-30 09:47:24 +0700

    Update uniswap_v3.js

--- uniswap_v3.js
+++ uniswap_v3.js
@@ -50,7 +50,7 @@ const checkPosition = async () => {
         console.log('Upper tick:', position.tickUpper.toString());
 
         if (!isInRange) {
-            notifyTelegram(`Position https://app.uniswap.org/positions/v3/ethereum/${positionId} is out of range ${currentTick >=  position.tickUpper ? 'break Upper Range ' : 'break Lower Range'}  currentTick: ${currentTick}`, process.env.TELEGRAM_TOKEN, process.env.TELEGRAM_CHAT)
+            notifyTelegram(`Position https://app.uniswap.org/positions/v3/${process.env.CHAIN}/${positionId} is out of range ${currentTick >=  position.tickUpper ? 'break Upper Range ' : 'break Lower Range'}  currentTick: ${currentTick}`, process.env.TELEGRAM_TOKEN, process.env.TELEGRAM_CHAT)
         }
     } catch (error) {
         console.error('Error checking position:', error);
